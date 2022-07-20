e1c36bb55fcaf6d6117baeec4f0441e6da06f814
JudyWang <>
2022-07-20 15:21:26 +0800

    delete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [data, setData] = useState([]);
   const [newData,setNewData] = useState("");
   const [list,setList] = useState([]);
+  const [count, setCount] = useState(0);
   //每次data有改變，就會變成字串，存入localStorage
   useEffect(()=>{
     localStorage.setItem("content",JSON.stringify(data));
@@ -20,16 +21,20 @@ function App() {
 
   //刪除
   const DeleteList = (id)=>{
-    console.log(id);
-    setData([...data.slice(0,id),...data.slice(id+1, data.length)]);
-    const content = localStorage.getItem("content");
-    const contents = JSON.parse(content);
-    setList(contents);
+    for(let i = 0; i < data.length; i++){
+      if(id === data[i].id){
+        setData([...data.slice(0,i),...data.slice(i+1, data.length)]);
+        const content = localStorage.getItem("content");
+        const contents = JSON.parse(content);
+        setList(contents);
+      }
+    }
   }
   const setNewLocals = ()=>{
+    setCount(count+1);
     const newList ={
         newlist : newData,
-        id: list.length
+        id: count
     }
     setData(data=>[...data,newList]);
      //localStorage.setItem("content",data);
@@ -38,11 +43,11 @@ function App() {
     <div className="App">
       <div className='head'>
         <h3>ToDoList</h3>
-        <input type="text" onChange={(e)=>{setNewData(e.target.value)}} />
+        <input type="text" onChange={(e)=>{setNewData(e.target.value);}} />
         <button onClick={()=>{setNewLocals()}}>add New</button>
         <ul>
           {list.map(item=>
-            <li key={item.id}><span className='number'>{item.id+1}</span>{item.newlist} <button onClick={()=>{DeleteList(item.id)}}>clear</button>
+            <li key={item.id}><span className='number'>{item.id}</span>{item.newlist} <button onClick={()=>{DeleteList(item.id)}}>clear</button>
             </li>               
           )}
         </ul>
